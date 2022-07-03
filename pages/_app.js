48347b3aa602bcf3d4a9b2d4c02a8ba1e23e29f8
Bibek Aryal <53701945+arlbibek@users.noreply.github.com>
2022-07-03 17:51:51 +0545

    Updated

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,7 @@
 import "../styles/globals.css";
-import { useEffect, useState } from "react";
 
-export default function MyApp({ Component, pageProps }) {
-  const [showChild, setShowChild] = useState(false);
-  useEffect(() => {
-    setShowChild(true);
-  }, []);
-
-  if (!showChild) {
-    return null;
-  }
-
-  if (typeof window === "undefined") {
-    return <></>;
-  } else {
-    return <Component {...pageProps} />;
-  }
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
+
+export default MyApp;
