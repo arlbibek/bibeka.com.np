f994e641df0424bc09c5423ed5b30aee54ac7492
bibekaryal <53701945+arlbibek@users.noreply.github.com>
2023-01-03 13:08:08 +0545

    Removed Underdev

--- app/projects/[projectId]/page.tsx
+++ app/projects/[projectId]/page.tsx
@@ -1,11 +1,25 @@
 "use client";
-
-import Underdev from "../../Components/underdev";
+import Link from "next/link";
 
 export default function ProjectPage() {
   return (
-    <>
-      <Underdev backto="projects" />
-    </>
+    <section className="flex items-center h-screen p-16 dark:bg-slate-900 dark:text-gray-100">
+      <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
+        <div className="max-w-md text-center">
+          <h2 className="mb-8 font-extrabold text-9xl dark:text-gray-600">
+            <span className="sr-only">Error</span>404
+          </h2>
+          <p className="text-2xl mb-8 font-semibold md:text-3xl">
+            the page is under development.
+          </p>
+          <Link
+            href="/projects"
+            className="px-8 py-3 font-semibold rounded bg-violet-500 text-white  dark:text-gray-900"
+          >
+            Back to /projects
+          </Link>
+        </div>
+      </div>
+    </section>
   );
 }
