7bfc998c5312093f5eb1b69274892a031a5f1f5a
bibek <contact@bibeka.com.np>
2023-09-20 17:55:15 +0545

    added redirects

--- next.config.js
+++ next.config.js
@@ -17,4 +17,30 @@ module.exports = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: "/install",
+        destination:
+          "https://raw.githubusercontent.com/arlbibek/scripts/master/install-from-dir.ps1",
+        permanent: true,
+      },
+      {
+        source: "/github",
+        destination: "https://github.com/arlbibek",
+        permanent: true,
+      },
+      {
+        source: "/easyaccess",
+        destination:
+          "https://drive.google.com/drive/folders/1F_OYMSpHlls1ej4KZSX6g2BDkKGt446T",
+        permanent: true,
+      },
+      {
+        source: "/easyupload",
+        destination: "https://mega.nz/megadrop/fzUsZP-DxgM",
+        permanent: true,
+      },
+    ];
+  },
 };
